245d2cba2eb286eee3d064e300d07609cedc08a0
v88-silver-roi <119904079+v88-silver-roi@users.noreply.github.com>
2022-12-28 16:54:06 +0800

    update - spawning of enemies

--- assets/js/tower_defense.js
+++ assets/js/tower_defense.js
@@ -76,13 +76,10 @@ var world5 = [
     [2,1,0,0,0,1,2,2,2,1,0,0,0],
     [2,1,1,1,1,1,2,2,2,1,1,1,1]
 ];
-
-let STARTING_LIFE = 40;
-let STARTING_RESOURCES = 8;
-
+const TILE_SIZE = 40;
 let basic_tower = () => {
     return {
-        title: "tower",
+        title: "basic_tower",
         cost: 2,
         range: 4,
         damage: 1,
@@ -115,9 +112,40 @@ let normal_bug = () => {
         title: "normal_bug",
         life: 5,
         damage: 1,
-        bounty: 1
+        bounty: 1,
+        x: starting_point_x,
+        y: starting_point_y,
+        right: right_x,
+        moveEnemy: function (){
+            /** check next tile location */
+
+            let mov_pattern = [2, 1, 1, 1, 1, 1];
+            
+            for(var mov_id = 0; mov_id < mov_pattern.length; mov_id++){
+                console.log(mov_pattern[mov_id]);
+                if(mov_pattern[mov_id] === 2){
+                    this.x += ENEMIES_SPEED;
+                }
+                else if(mov_pattern[mov_id === 1]){
+                    this.y -= ENEMIES_SPEED;
+                }
+            }
+ 
+            const move_enemy = setTimeout(() => {
+                this.moveEnemy();
+            }, 1000);
+        }
     };
 }
+
+let STARTING_LIFE = 20;
+let STARTING_RESOURCES = 8;
+let MAX_ENEMIES = 5;
+let ENEMIES_SPEED = 1.05;
+let enemies = [];
+let starting_point_x = 0;
+let starting_point_y = 0;
+let right_x = 0;
 
 spawnRandomWorld();
 
@@ -137,8 +165,16 @@ function spawnRandomWorld(){
         world = world5;
     }
 
-    document.getElementById("life_points").querySelector("span").textContent = STARTING_LIFE;
+    const life_points = document.getElementById("life_points").querySelector("span");
+    life_points.textContent = STARTING_LIFE;
     document.getElementById("resources").querySelector("span").textContent = STARTING_RESOURCES;
+
+    if(life_points.textContent <= 30 && life_points.textContent >= 21){
+        life_points.classList.add("orange");
+    }
+    else if(life_points.textContent <= 20){
+        life_points.classList.add("red");
+    }
 }
 
 function displayWorld(){
@@ -156,7 +192,6 @@ function displayWorld(){
         }
         output += "\n</tr>";
     }
-    // console.log(output);
     document.getElementById('world').innerHTML = output;
 }
 
@@ -173,23 +208,50 @@ function activeTower(){
 
 function spawnEnemies(){
     const soil_tiles = document.querySelectorAll('.soil');
-
     soil_tiles.forEach(soil => {
         var rect = soil.getBoundingClientRect();
         if (rect.left === 0){
-            let starting_point_id = soil.getAttribute("id");
-            console.log(starting_point_id);
+            starting_point_x = rect.left;
+            starting_point_y = rect.top + 1;
+            right_x = rect.right;
         }
     });
+    // console.log(starting_point_x);
+    // console.log(starting_point_y);
+    /** use for loop to initialize enemies */
+    if(enemies.length < MAX_ENEMIES){
+        let enemy_spawn = normal_bug();
+        
+        enemy_spawn.y = (Math.floor(Math.random() * 700) + 1);
+        enemies.push(enemy_spawn);
+        // enemy_spawn.shootBullet();
+        enemy_spawn.moveEnemy();
+        setTimeout(spawnEnemies, 2000); 
+    }
+}
+
+function drawEnemies(){
+    let output = "";
+    for(let enemy_id = 0; enemy_id < enemies.length; enemy_id++){
+        output += `<div class='${ enemies[enemy_id].title } right_front_bug ' style='top: ${ starting_point_y }px; left: ${ enemies[enemy_id].x * TILE_SIZE }px;'></div>`; 
+        // if(enemies[enemy_id].x < -65){
+        //     enemies.shift();
+        // }
+    } 
+    document.getElementById("enemies").innerHTML = output;
 }
 
 function gameloop(){
+    /** All functions that will display something go here */
     displayWorld();
     activeTower();
-    spawnEnemies();
+    drawEnemies();
+
+    setTimeout(gameloop, 16);
 }
 
-setTimeout(gameloop, 5)
+gameloop();
+setTimeout(spawnEnemies, 1000);
 
 let world_field = document.getElementById("world");
 
@@ -197,6 +259,7 @@ world_field.addEventListener("click", event => {
     let active_tower_id = document.getElementById("tower_buttons").querySelector(".active").getAttribute("id");
     let tower_clone = document.getElementById(active_tower_id).querySelector("img").cloneNode(true);
     let selected_tile = event.target.getAttribute("class");
+    let element = event.target.childElementCount;
     
     // function getOffset(element) {
     //     const rect = element.getBoundingClientRect();
@@ -213,33 +276,35 @@ world_field.addEventListener("click", event => {
     // var rect = event.target.getBoundingClientRect();
     // console.log(rect.left);
 
-    if(active_tower_id === "tower" && selected_tile === "grass"){
-        let tower = basic_tower();
-
-        if(STARTING_RESOURCES >= 2){
-            event.target.append(tower_clone);
-            STARTING_RESOURCES -= tower.cost;
-            document.getElementById("resources").querySelector("span").textContent = STARTING_RESOURCES;
+    if(element === 0){
+        if(active_tower_id === "tower" && selected_tile === "grass"){
+            let tower = basic_tower();
+    
+            if(STARTING_RESOURCES >= 2){
+                event.target.append(tower_clone);
+                STARTING_RESOURCES -= tower.cost;
+                document.getElementById("resources").querySelector("span").textContent = STARTING_RESOURCES;
+            }
         }
-    }
-
-    else if(active_tower_id === "control_tower" && selected_tile === "grass"){
-        let tower = control_tower();
-
-        if(STARTING_RESOURCES >= 2){
-            event.target.append(tower_clone);
-            STARTING_RESOURCES -= tower.cost;
-            document.getElementById("resources").querySelector("span").textContent = STARTING_RESOURCES;
+    
+        else if(active_tower_id === "control_tower" && selected_tile === "grass"){
+            let tower = control_tower();
+    
+            if(STARTING_RESOURCES >= 2){
+                event.target.append(tower_clone);
+                STARTING_RESOURCES -= tower.cost;
+                document.getElementById("resources").querySelector("span").textContent = STARTING_RESOURCES;
+            }
         }
-    }
-
-    else if(active_tower_id === "sea_tower" && selected_tile === "water"){
-        let tower = sea_tower();
-
-        if(STARTING_RESOURCES >= 5){
-            event.target.append(tower_clone);
-            STARTING_RESOURCES -= tower.cost;
-            document.getElementById("resources").querySelector("span").textContent = STARTING_RESOURCES;
+    
+        else if(active_tower_id === "sea_tower" && selected_tile === "water"){
+            let tower = sea_tower();
+    
+            if(STARTING_RESOURCES >= 5){
+                event.target.append(tower_clone);
+                STARTING_RESOURCES -= tower.cost;
+                document.getElementById("resources").querySelector("span").textContent = STARTING_RESOURCES;
+            }
         }
     }
 
@@ -260,3 +325,8 @@ world_field.addEventListener("click", event => {
         }
     }
 });
+
+function enemyMovement(){
+    // if(world[this.y][this.x] == 0){
+    // }
+}
